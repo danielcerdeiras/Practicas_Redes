f5278f8dc05c97f9e4992c6e6c8b7bf5d211c7b5
Daniel González Cerdeiras <daniel20@ucm.es>
2021-05-28 18:57:50 +0200

    Hecho server de clientes. Por lo menos compila

--- Practica_2.2/practica2.2/replicacion-chat/Chat.cc
+++ Practica_2.2/practica2.2/replicacion-chat/Chat.cc
@@ -11,13 +11,13 @@ void ChatMessage::to_bin()
 
     char* tmp = _data;
 
-    memcpy(tmp, type, sizeof(uint8_t));
+    memcpy(tmp, &type, sizeof(uint8_t));
     tmp += sizeof(uint8_t);
 
-    memcpy(tmp, nick, 8 * sizeof(char));
+    memcpy(tmp, &nick, 8 * sizeof(char));
     tmp += 8 * sizeof(char);
 
-    memcpy(tmp, message, 80 * sizeof(char));
+    memcpy(tmp, &message, 80 * sizeof(char));
 }
 
 int ChatMessage::from_bin(char * bobj)
@@ -28,13 +28,13 @@ int ChatMessage::from_bin(char * bobj)
 
     char* tmp = _data;
 
-    memcpy(type, tmp, sizeof(int16_t));
+    memcpy(&type, tmp, sizeof(int16_t));
     tmp += sizeof(int16_t);
 
-    memcpy(nick, tmp, 8 * sizeof(char));
-    tmp += 8 sizeof(char);
+    memcpy(&nick, tmp, 8 * sizeof(char));
+    tmp += 8 * sizeof(char);
 
-    memcpy(message, tmp, 80 * sizeof(char));
+    memcpy(&message, tmp, 80 * sizeof(char));
 
     return 0;
 }
@@ -57,7 +57,38 @@ void ChatServer::do_messages()
         // - LOGOUT: Eliminar del vector clients
         // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
 
-        
+        ChatMessage mess;
+        Socket* client;
+        socket.recv(mess, client);
+        switch (mess.type)
+        {
+        case ChatMessage::LOGIN:
+            clients.push_back(std::move(std::unique_ptr<Socket>(client)));
+            break;
+        case ChatMessage::LOGOUT:
+            {
+            auto it = clients.begin();
+            while (it != clients.end()){
+                if (it->get() == client) {
+                    clients.erase(it);
+                    it->release();
+                    break;
+                }
+                ++it;
+            }
+            break;
+            }
+        case ChatMessage::MESSAGE:
+            {
+            auto it = clients.begin();
+            while (it != clients.end()){
+                if (it->get() == client) continue;
+                it->get()->send(mess, *client);
+                ++it;
+            }
+            break;
+            }
+        }
     }
 }
 
